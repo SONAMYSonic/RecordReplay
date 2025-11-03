1bf0cf4d7c2ab87d2a854c7c5ec6479759709234
AE86T <38212834+SONAMYSonic@users.noreply.github.com>
2025-11-03 10:08:30 +0900

    251103_1008
    MTE

---
+++ Assets/Scripts/RecordReplay.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class RecordReplay : MonoBehaviour
+{
+    // 플레이어의 입력을 기록하고 재생하는 기능
+
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+}
